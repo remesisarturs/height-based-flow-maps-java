f6516963453adfaf032993e36772c5949c9d1b9c
Arturs <remesis.arturs@gmail.com>
2022-03-21 23:11:14 +0100

    Added function to translate points to grid coordinates

--- Height based flow maps/src/Main.java
+++ Height based flow maps/src/Main.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.*;
 
 public class Main {
@@ -27,8 +25,46 @@ public class Main {
 
         Bounds bounds = obtain_bounds(points_list);
 
+        compute_cell_for_point(bounds, nr_of_rows, nr_of_columns, points_list);
+
+
+
         System.out.println();
 
+
+    }
+
+    public static void compute_cell_for_point(Bounds bounds, int nr_of_rows, int nr_of_columns, ArrayList points) {
+
+        Iterator it = points.iterator();
+
+        double extent_x = bounds.max_x - bounds.min_x;
+        double extent_y = bounds.max_y - bounds.min_y;
+
+        double step_x = extent_x / nr_of_columns;
+        double step_y = extent_y / nr_of_rows;
+
+        while (it.hasNext()) {
+
+            Point point = (Point) it.next();
+
+            int i = 1;
+            int j = 1;
+
+            while (bounds.min_x + i * step_x < point.x) {
+                i = i + 1;
+            }
+            int column_index = i - 1;
+
+            while (bounds.min_y + j * step_y < point.y) {
+                j = j + 1;
+            }
+            int row_index = j - 1;
+
+            point.grid_x = column_index;
+            point.grid_y = row_index;
+
+        }
 
     }
 
--- Height based flow maps/src/Point.java
+++ Height based flow maps/src/Point.java
@@ -3,5 +3,7 @@ public class Point {
     float x;
     float y;
     String name;
+    int grid_x;
+    int grid_y;
 
 }
