b9e5d7cd57e53775cb577fb58d9b190451029b5e
Arturs <remesis.arturs@gmail.com>
2022-03-21 16:46:56 +0100

    Made method for initializing grid

--- Height based flow maps/src/Main.java
+++ Height based flow maps/src/Main.java
@@ -5,17 +5,25 @@ public class Main {
 
         System.out.println("test");
 
-        Cell cell = new Cell();
+        //Cell cell = new Cell();
 
         int nr_of_rows = 100;
         int nr_of_columns = 100;
 
-        Cell[][] grid = new Cell[nr_of_rows][nr_of_columns];
+        Cell[][] grid = initialize_grid(nr_of_rows, nr_of_columns);
+        
 
-        for (int i = 0; i < nr_of_rows; i++) {
-            for (int j = 0; j < nr_of_columns; j++) {
 
-                cell = new Cell();
+    }
+
+    public static Cell[][] initialize_grid(int number_of_rows, int number_of_columns){
+
+        Cell[][] grid = new Cell[number_of_rows][number_of_columns];
+
+        for (int i = 0; i < number_of_rows; i++) {
+            for (int j = 0; j < number_of_columns; j++) {
+
+                Cell cell = new Cell();
 
                 cell.x = 0;
                 cell.y = 0;
@@ -26,11 +34,12 @@ public class Main {
 
                 grid[i][j] = cell;
 
-                System.out.println(grid[i][j].temp);
+                //System.out.println(grid[i][j].temp);
                 //grid[i][j]
             }
         }
 
+        return grid;
     }
 
 }
